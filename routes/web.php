ce9bce61c42d93a47aaea80a702c39e8cd785acf
LouisSetyandaru <lsetyandaru@student.ciputra.ac.id>
2024-05-31 15:13:15 +0700

    Commit buat Login - Logout
    buat login logput tinggal view

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,27 @@
 <?php
 
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\SesiController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::middleware(['guest'])->group(function(){
+    Route::get('/', [SesiController::class,'index'])->name('login');
+    Route::post('/', [SesiController::class,'login']);
 });
+// Route::get('/home', function ()){
+//     return
+// }
+
+Route::middleware(['auth'])->group(function(){
+    Route::get('/admin', [AdminController::class,'index']);
+    Route::get('/admin/customer', [AdminController::class,'customer']);
+    Route::get('/admin/admin', [AdminController::class,'admin']);
+    Route::get('/admin/owner', [AdminController::class,'owner']);
+    Route::get('/logout', [SesiController::class,'logout']);
+});
+
+
